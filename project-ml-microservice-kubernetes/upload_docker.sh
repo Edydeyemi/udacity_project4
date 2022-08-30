72c555eeb6718f0c8f130dae60b5215e10e095af
Edydeyemi <ubuntu@ip-172-31-3-192.us-east-2.compute.internal>
2022-08-30 22:37:38 +0000

    Push  image to DockerHub

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -5,12 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-dockerpath=edydeyemi/mli
+dockerpath="edydeyemi/noobmli"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login &&\
+    docker image tag noobmli $dockerpath
 
 # Step 3:
 # Push image to a docker repository
-docker image push edydeyemi/mli
+docker image push $dockerpath
